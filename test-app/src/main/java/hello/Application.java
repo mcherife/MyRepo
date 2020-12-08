439236031ddd70006b6751c9502e262494b25e4e
mcherife <55875130+mcherife@users.noreply.github.com>
2020-12-08 22:41:16 +0100

    Ajout modifications

---
+++ test-app/src/main/java/hello/Application.java
@@ -0,0 +1,24 @@
+package hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.bind.RelaxedPropertyResolver;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class Application {
+
+	@RequestMapping("/")
+	public String home() {
+		// Add comments
+		return "Hello cherif  World from OpenShift (v2)";
+	}
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
